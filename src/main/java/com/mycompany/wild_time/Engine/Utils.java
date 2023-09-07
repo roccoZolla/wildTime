96b9b7b34926e3bccaba55a8e5503a423202d596
Rocco Piazzolla <roccopiazzo2001@gmail.com>
2023-09-07 18:48:05 +0200

    aggiornata classe Utils, aggiunta metodo Final e bug fix

--- src/main/java/com/mycompany/wild_time/Engine/Utils.java
+++ src/main/java/com/mycompany/wild_time/Engine/Utils.java
@@ -20,9 +20,6 @@ import java.io.ObjectOutputStream;
  */
 
 public class Utils {
-    
-    
-    
     /* public static Set<String> loadFileListInSet(File file) throws IOException {
         Set<String> set = new HashSet<>();
         BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -32,17 +29,7 @@ public class Utils {
         reader.close();
         return set;
     }
-
-    public static List<String> parseString(String string, Set<String> stopwords) {
-        List<String> tokens = new ArrayList<>();
-        String[] split = string.toLowerCase().split("\\s+");
-        for (String t : split) {
-            if (!stopwords.contains(t)) {
-                tokens.add(t);
-            }
-        }
-        return tokens;
-    } */
+*/
     
     public static String LoadGame(GameDescription game, File saveFile) {
         String text = "";
@@ -79,38 +66,37 @@ public class Utils {
         if(p.getCommand() == null) {
             text = "Non capisco quello che mi vuoi dire";
         } else {
-            if (p.getCommand().getType() == CommandType.NORD) {
+            if (p.getCommand().getType().equals(CommandType.NORD)) {
                 if (player1.getCurrentPlace().getNord() != null && !player1.getCurrentPlace().getNord().getBlocked()) {
                     player1.setCurrentPlace(player1.getCurrentPlace().getNord());
                     text = player1.getCurrentPlace().getDescription();
                 } else {
                     text = "Non c'è niente da questa parte";
                 } 
-            } else if (p.getCommand().getType() == CommandType.SOUTH) {
+            } else if (p.getCommand().getType().equals(CommandType.SOUTH)) {
                 if (player1.getCurrentPlace().getSouth() != null && !player1.getCurrentPlace().getSouth().getBlocked()) {
                     player1.setCurrentPlace(player1.getCurrentPlace().getSouth());
                     text = player1.getCurrentPlace().getDescription();
                 } else {
                     text = "Non c'è niente da questa parte";
                 }
-            } else if (p.getCommand().getType() == CommandType.EAST) {
+            } else if (p.getCommand().getType().equals(CommandType.EAST)) {
                 if (player1.getCurrentPlace().getEst() != null && !player1.getCurrentPlace().getEst().getBlocked()) {
                     player1.setCurrentPlace(player1.getCurrentPlace().getEst());
                     text = player1.getCurrentPlace().getDescription();
                 } else {
                     text = "Non c'è niente da questa parte";
                 }
-            } else if (p.getCommand().getType() == CommandType.WEST) {
+            } else if (p.getCommand().getType().equals(CommandType.WEST)) {
                 if (player1.getCurrentPlace().getWest() != null && !player1.getCurrentPlace().getWest().getBlocked()) {
                     player1.setCurrentPlace(player1.getCurrentPlace().getWest());
                     text = player1.getCurrentPlace().getDescription();
                 } else {
                     text = "Non c'è niente da questa parte";
                 } 
-            } else if (p.getCommand().getType() == CommandType.PICK_UP) {                  // pick up
+            } else if (p.getCommand().getType().equals(CommandType.PICK_UP)) {                  // pick up
                 // Aggiungere controlli relativi agli oggetti
                 // si puo raccogliere?
-                
                 
                 // System.out.println(p.getObject().getName() + p.getObject().isTakeable());
                 
@@ -132,13 +118,19 @@ public class Utils {
                         if(p.getObject().equals(player1.getCurrentPlace().getItems().get(i))) {                           
                             System.out.println("oggetto for: " + player1.getCurrentPlace().getItems().get(i).getName());
                             player1.getInventory().getList().add(player1.getCurrentPlace().getItems().get(i));
-                            text = "Hai raccolto " + player1.getCurrentPlace().getItems().get(i).getName();
-                            // text += ". " + player1.getCurrentPlace().getItems().get(i).getDescription();
-                            player1.getCurrentPlace().getItems().remove(i);                           
+                            text = "Hai raccolto: " + player1.getCurrentPlace().getItems().get(i).getName();
+                            text += ". " + player1.getCurrentPlace().getItems().get(i).getDescription();
+                            player1.getCurrentPlace().getItems().remove(i);
                         }
                     }
+                    
+                    // se sono stati raccolti tutti gli oggetti la stanza risulta essere esplorata  
+                    // al momento con questo metodo è inutile perche le stanze con le casse non risulteranno mai vuote
+                    if(player1.getCurrentPlace().getItems().isEmpty()) 
+                        player1.getCurrentPlace().setExplored(true);
+                    
                 } 
-            } else if (p.getCommand().getType() == CommandType.LOOK_AT) {                  // guarda
+            } else if (p.getCommand().getType().equals(CommandType.LOOK_AT)) {                  // guarda
                 if(!player1.getCurrentPlace().getNpcs().isEmpty()){
                     for(int i = 0; i < player1.getCurrentPlace().getNpcs().size(); i++) {
                         System.out.println("Lista npc:" + player1.getCurrentPlace().getNpcs().get(i).getName());
@@ -146,7 +138,7 @@ public class Utils {
                 }
                 
                 // cosa c'è nella stanza?
-                if (!player1.getCurrentPlace().getItems().isEmpty() || !player1.getCurrentPlace().getChest().getChest().isEmpty()) {
+                if (!player1.getCurrentPlace().getItems().isEmpty() || !player1.getCurrentPlace().getChest().getList().isEmpty()) {
                     text = "Nella stanza sono presenti: ";
                     for(int i = 0; i < player1.getCurrentPlace().getItems().size(); i++) {
                         text += player1.getCurrentPlace().getItems().get(i).getName() + " ";
@@ -159,17 +151,17 @@ public class Utils {
                 
                 if(!player1.getCurrentPlace().getNpcs().isEmpty()){
                     // guarda se sono presenti npcs
-                        text += "\nOh, ma c'è qualcuno qui: ";
+                        text += "\nOh, ma c'è qualcuno qui:\n";
                     
                     for(int i = 0; i < player1.getCurrentPlace().getNpcs().size(); i++) {
                         if(player1.getCurrentPlace().getNpcs().get(i).getHp() <= 0) {
                             text += "+" + player1.getCurrentPlace().getNpcs().get(i).getName() + "+";
                         } else {
-                            text += player1.getCurrentPlace().getNpcs().get(i).getName() + " ";
+                            text += player1.getCurrentPlace().getNpcs().get(i).getName() + ". " + player1.getCurrentPlace().getNpcs().get(i).getDescription() + "\n";
                         }
                     }
                 }
-            } else if(p.getCommand().getType() == CommandType.INVENTORY) {
+            } else if(p.getCommand().getType().equals(CommandType.INVENTORY)) {
                 // cosa ho nell'inventario
                 
                 if(!player1.getInventory().getList().isEmpty()) {
@@ -185,7 +177,7 @@ public class Utils {
                 } else {
                     text = "Non hai niente nello zaino!";
                 }
-            } else if(p.getCommand().getType() == CommandType.OPEN) {
+            } else if(p.getCommand().getType().equals(CommandType.OPEN)) {
                 // apri le cose
                 if(p.getObject() == null) {
                     text = "Non è l'oggetto giusto per aprire qualcosa...";                  
@@ -195,12 +187,12 @@ public class Utils {
                     if(p.getObject().isUseable() && p.getChestObject().getKey().equals(p.getObject())) {
                         text = "Hai aperto la cassa! \nLa cassa contiene: ";
                         
-                        for(int i = 0; i < player1.getCurrentPlace().getChest().getChest().size(); i++)  {
+                        for(int i = 0; i < player1.getCurrentPlace().getChest().getList().size(); i++)  {
                             // ottengo gli elementi presenti nella cassa e li inserisco
                             // nella lista degli oggetti presenti nella stanza
-                            player1.getCurrentPlace().getItems().add(player1.getCurrentPlace().getChest().getChest().get(i));
+                            player1.getCurrentPlace().getItems().add(player1.getCurrentPlace().getChest().getList().get(i));
                             
-                            text += player1.getCurrentPlace().getChest().getChest().get(i).getName();
+                            text += player1.getCurrentPlace().getChest().getList().get(i).getName();
                         }    
                         
                         // la cassa risultera aperta
@@ -211,13 +203,13 @@ public class Utils {
                         player1.getInventory().getList().get(n).setUseable(false);
                             
                         // rimuovo tutti gli oggetti presenti nella cassa
-                        player1.getCurrentPlace().getChest().getChest().removeAll(player1.getCurrentPlace().getChest().getChest());
+                        player1.getCurrentPlace().getChest().getList().removeAll(player1.getCurrentPlace().getChest().getList());
    
                     } else {
                         text = "Non è possbile aprire questo oggetto";
                     }
                 }        
-            } else if(p.getCommand().getType() == CommandType.EAT) {
+            } else if(p.getCommand().getType().equals(CommandType.EAT)) {
                 // permette di utilizzare gli oggetti presenti nell'inventario
                 // oggetti di tipo curativo
                 
@@ -240,11 +232,11 @@ public class Utils {
                     
                     text = "Hai recuperato " + p.getObject().getBonusHP() + " HP";
                 }
-            } else if (p.getCommand().getType() == CommandType.EQUIP) {
+            } else if (p.getCommand().getType().equals(CommandType.EQUIP)) {
                 // equipaggia le armi e gli scudi che trovi nel mondo di gioco
                 
-                System.out.println("Usesabilty: " + p.getObject().isUseable());
-                System.out.println("Weaponability: " + p.getObject().IsWeapon());
+                // System.out.println("Usesabilty: " + p.getObject().isUseable());
+                // System.out.println("Weaponability: " + p.getObject().IsWeapon());
                 
                 if(p.getObject() == null) {
                     text = "Il nulla non è un qualcosa che si puo usare...";
@@ -259,16 +251,16 @@ public class Utils {
                         player1.getInventory().getList().add(player1.getArma());
                         player1.setArma(p.getObject());
                         
-                        text = "Hai equipaggiato " + player1.getArma().getName();
+                        text = "Hai equipaggiato: " + player1.getArma().getName();
                     } else if (p.getObject().getDefenseBonus() > 0) {
                         player1.getInventory().getList().add(player1.getScudo());
                         player1.setScudo(p.getObject());
                         
-                        text = "Hai equipaggiato " + player1.getScudo().getName();
+                        text = "Hai equipaggiato: " + player1.getScudo().getName();
                     }
                 }
                 
-            } else if(p.getCommand().getType() == CommandType.ATTACK) {     
+            } else if(p.getCommand().getType().equals(CommandType.ATTACK)) {     
                 // attacca i nemici
                 int attack = 0;
                 int defense = 0;
@@ -292,7 +284,11 @@ public class Utils {
                     p.getNpc().setHp(p.getNpc().getHp() - damage);
                     
                     if(p.getNpc().getHp() <= 0){
-                        text = p.getNpc().getName().toUpperCase() + ": E' stato un piacere mercenario";
+                        text = p.getNpc().getName().toUpperCase() + ": E' stato un piacere mercenario\n";
+                        if(p.getNpc().getReward() != null) {
+                            text += p.getNpc().getName().toUpperCase() + " ha lasciato cadere: " + p.getNpc().getReward().getName();
+                            player1.getCurrentPlace().getItems().add(p.getNpc().getReward());
+                        }
                     } else {
                         text = p.getNpc().getName().toUpperCase() + " HP: " + p.getNpc().getHp() + "\n";
                         text += p.getNpc().getName().toUpperCase() + ": Tocca ammazzarti!";
@@ -307,8 +303,7 @@ public class Utils {
                         player1.setHp(player1.getHp() - damage);
                     }
                 }
-            } else if(p.getCommand().getType() == CommandType.TALK_TO) {  
-                // DA SISTEMARE: implementare un sistema di dialogo
+            } else if(p.getCommand().getType().equals(CommandType.TALK_TO)) { 
                 // parla con gli npc
                 if(p.getNpc() == null) {
                     text = "Non puoi parlare con qualcuno che non c'è...";
@@ -323,11 +318,42 @@ public class Utils {
                     text += "Per uscire dalla conversazione digita un altro comando\n";
                     text += p.getNpc().getName().toUpperCase() + ": " + p.getNpc().getTalk();
                 }
+            } else if(p.getCommand().getType().equals(CommandType.THROW)) {
+                // lascia un oggetto
+                if(p.getObject() == null) {
+                    text = "Non puoi lasciare qualcosa che non c'è...";
+                } else if(!player1.getInventory().getList().contains(p.getObject())) {
+                    text = "Non puoi lasciare qualcosa che non è presente nel tuo zaino..."; 
+                } else {
+                    // rimuovi l'oggetto dallo zaino
+                    player1.getInventory().getList().remove(p.getObject());
+                    
+                    // aggiungi l'oggetto rimosso alla stanza corrente
+                    player1.getCurrentPlace().getItems().add(p.getObject());
+                    
+                    text = "Hai lasciato: " + p.getObject().getName();
+                }
                 
             }
         }
         
         return text;
+    }
+    
+    public static boolean Final(Player player) {
+        boolean flag = false;
+        
+        if(player.getCurrentPlace().getFinal()) {
+            for(int i = 0; i < player.getCurrentPlace().getNpcs().size(); i++) {
+                // se è un nemico e la salute è a zero
+                if(player.getCurrentPlace().getNpcs().get(i).getIsEnemy() && player.getCurrentPlace().getNpcs().get(i).getHp() <= 0) {
+                    flag = true;
+                }
+            }
+        }
+            
+            
+        return flag;
     }
     
     
